7a0c762c7c8a0f7eb7010b5e9208fb976396112c
M-Loyola <m.yakit.loyola@gmail.com>
2023-08-30 01:35:51 +0800

    feat: modify and add checker for null input

--- src/components/TodoGenerator.js
+++ src/components/TodoGenerator.js
@@ -1,23 +1,27 @@
-import { useDispatch } from "react-redux";
-import { addTodoItem } from "./todoSlice";
-import "./css/TodoGenerator.css"
-import { useState } from "react";
-import { v4 as uuidv4 } from 'uuid';
 import { Button, Input, Space } from 'antd';
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { v4 as uuidv4 } from 'uuid';
+import "./css/TodoGenerator.css";
+import { addTodoItem } from "./todoSlice";
 
 const TodoGenerator = () => {
     const dispatch = useDispatch()
     const [inputValue, setInputValue] = useState("");
 
     const handleTodoItem = () => {
-        dispatch(addTodoItem(
-            {
-                id: uuidv4(),
-                text: inputValue,
-                done: false
-            }
-        ));
-        setInputValue("");
+        if (inputValue.trim() === '') {
+            window.confirm("Input can't be blank");
+        } else {
+            dispatch(addTodoItem(
+                {
+                    id: uuidv4(),
+                    text: inputValue,
+                    done: false
+                }
+            ));
+            setInputValue("");
+        }
     }
 
     return (
