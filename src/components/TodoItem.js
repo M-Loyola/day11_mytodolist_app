091bc091d6fd0cb6d1e62a2d48eec9dc36b2f48c
M-Loyola <m.yakit.loyola@gmail.com>
2023-08-30 19:05:34 +0800

    feat: add update modal for TodoItem js

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,5 +1,5 @@
 import { CloseOutlined } from "@ant-design/icons";
-import { Card, Col, Modal, Row } from "antd";
+import { Card, Col, Input, Modal, Row } from "antd";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import * as todoApi from "../api/todoApi";
@@ -8,13 +8,23 @@ import { resetTodoList } from "./todoSlice";
 
 const TodoItem = (props) => {
     const dispatch = useDispatch();
+    const [updateValue, setUpdateValue] = useState("");
     const [isDeleteModalVisible, setIsDeleteModalVisible] = useState(false);
+    const [isUpdateModalVisible, setIsUpdateModalVisible] = useState(false);
 
     const onTriggerToggle = async () => {
-        await todoApi.updateTodoTask(props.todo.id, {done: !props.todo.done});
+        await todoApi.updateTodoTask(props.todo.id, { done: !props.todo.done });
         const response = await todoApi.getTodoTasks();
         dispatch(resetTodoList(response.data));
     };
+
+    const handleUpdateConfirm = async () => {
+        await todoApi.updateTodoTask(props.todo.id, { text: updateValue });
+        const response = await todoApi.getTodoTasks();
+        dispatch(resetTodoList(response.data));
+        setIsUpdateModalVisible(false);
+        setUpdateValue("");
+    }
 
     const handleDeleteConfirm = async () => {
         await todoApi.deleteTodoTask(props.todo.id);
@@ -25,6 +35,15 @@ const TodoItem = (props) => {
 
     const showDeleteModal = () => {
         setIsDeleteModalVisible(true);
+    };
+
+    const showUpdateModal = () => {
+        setIsUpdateModalVisible(true);
+    };
+
+    const handleUpdateCancel = () => {
+        setUpdateValue("");
+        setIsUpdateModalVisible(false);
     };
 
     const handleDeleteCancel = () => {
@@ -38,6 +57,9 @@ const TodoItem = (props) => {
             <Row justify="space-between" align="middle" gutter={[16, 8]}>
                 <Col onClick={onTriggerToggle}><p>{currentDate}: {props.todo.text}</p></Col>
                 <Col style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                    <button className="updateButton" onClick={showUpdateModal}><CloseOutlined /></button>
+                </Col>
+                <Col style={{ display: 'flex', justifyContent: 'flex-end' }}>
                     <button className="deleteButton" onClick={showDeleteModal}><CloseOutlined /></button>
                 </Col>
             </Row>
@@ -48,6 +70,19 @@ const TodoItem = (props) => {
                 onCancel={handleDeleteCancel}
             >
                 <p>Do you want to delete this?</p>
+            </Modal>
+            <Modal
+                title="Update Todo"
+                open={isUpdateModalVisible}
+                onOk={handleUpdateConfirm}
+                onCancel={handleUpdateCancel}
+            >
+                <p>What do you want to update?</p>
+                <Input
+                    value={updateValue}
+                    onChange={(input) => setUpdateValue(input.target.value)}
+                    placeholder="Please input here"
+                />
             </Modal>
         </Card>
     );
