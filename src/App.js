daf7e5cd3c0eddf510baac573103a8d06e36579a
M-Loyola <m.yakit.loyola@gmail.com>
2023-08-30 11:23:06 +0800

    feat: modify nav css and add help page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,18 @@ import TodoList from "./components/TodoList";
 
 function App() {
   return (
+    <>
+    <div className="nav-bar">
+      <nav>
+        <ul style={{display:"flex", listStyleType: "none", justifyContent: "space-evenly"}}>
+          <li><a  href={'/'}>Home</a></li>
+          <li><a  href={'/done'}>Done List</a></li>
+          <li><a  href={'/help'}>Help</a></li>
+        </ul>
+      </nav>
+    </div>
     <TodoList />
+    </>
   );
 }
 
---
+++ src/pages/HelpPage.js
@@ -0,0 +1,7 @@
+export const HelpPage = () => {
+    return (
+        <div>
+            <h1>Help  me</h1>
+        </div>
+    );
+}
