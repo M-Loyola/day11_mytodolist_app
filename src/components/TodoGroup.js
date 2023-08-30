82756ee50c5cc876da0dfd1a5a6dc4c9471e1c0a
M-Loyola <m.yakit.loyola@gmail.com>
2023-08-30 10:20:34 +0800

    refactor: remove element and utilize todo id

--- src/components/TodoGroup.js
+++ src/components/TodoGroup.js
@@ -4,8 +4,8 @@ import "./css/TodoGroup.css";
 const TodoGroup = (props) => {
     return (
         <ul className="todoGroup">
-            {props.todoList.map((todo, index) => (
-                <TodoItem key={index} todo={todo} />
+            {props.todoList.map((todo) => (
+                <TodoItem key={todo.id} todo={todo} />
             ))}
         </ul>
     );
