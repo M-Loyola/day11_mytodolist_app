ecffc2be1b44a16279b75fb5a70825a3c68c04e2
M-Loyola <m.yakit.loyola@gmail.com>
2023-08-30 14:27:34 +0800

    refactor: remove unneeded codes

--- src/components/DoneGroup.js
+++ src/components/DoneGroup.js
@@ -5,7 +5,7 @@ export const DoneGroup = (props) => {
     const todoList = props.todoList;
     return (
         <ul className="todoGroup">
-            {(props.isDone ? todoList.filter(task=> task.done) : todoList).map((todo) => (
+            {todoList.map((todo) => (
                 <DoneItem key={todo.id} todo={todo} done={todo.done} isDone={props.isDone} />
             ))}
         </ul>
