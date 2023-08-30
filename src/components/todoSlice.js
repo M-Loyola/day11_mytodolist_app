51383e8c27ba0758f7310545cbe973b1d0bc8d9d
M-Loyola <m.yakit.loyola@gmail.com>
2023-08-30 17:07:38 +0800

    refactor: remove unneeded reducers in todoSlice

--- src/components/todoSlice.js
+++ src/components/todoSlice.js
@@ -9,22 +9,9 @@ const todoSlice = createSlice({
       resetTodoList: (state, action) => {
          state.todoList = action.payload;
       },
-      addTodoItem: (state, action) => {
-         state.todoList.push(action.payload);
-      },
 
-      onToggle: (state, action) => {
-         const todoToUpdate = state.todoList.find((todo) => todo.id === action.payload);
-         if (todoToUpdate) {
-            todoToUpdate.done = !todoToUpdate.done;
-         }
-      },
-
-      deleteTodoItem: (state, action) => {
-         state.todoList = state.todoList.filter((todo) => todo.id !== action.payload);
-      }
    }
 });
 
-export const { addTodoItem, onToggle, deleteTodoItem , resetTodoList} = todoSlice.actions;
+export const { resetTodoList } = todoSlice.actions;
 export default todoSlice.reducer;
