66f222b015218df4925c9c11e9045bfbc99de8b7
M-Loyola <m.yakit.loyola@gmail.com>
2023-08-30 11:06:47 +0800

    feat: install react router and using router provider  for application

--- src/index.js
+++ src/index.js
@@ -4,12 +4,18 @@ import { Provider } from 'react-redux';
 import App from './App';
 import { store } from './app/store';
 import reportWebVitals from './reportWebVitals';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([{
+  path: "/",
+  element: <App/>
+}]);
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
