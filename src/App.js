0a69f8eaa3a6b3e45a7e57833d9b007ab285f27c
M-Loyola <m.yakit.loyola@gmail.com>
2023-08-30 13:14:11 +0800

    feat: modify nav and use Menu antd

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
+import { Menu } from "antd";
 import { Outlet, NavLink } from "react-router-dom";
+import  './App.css';
 
 function App() {
   return (
     <>
       <div className="nav-bar">
-        <nav>
-          <ul style={{ display: "flex", listStyleType: "none", justifyContent: "space-evenly" }}>
-            <li><NavLink to={'/'}>Home</NavLink></li>
-            <li><NavLink to={'/done'}>Done List</NavLink></li>
-            <li><NavLink to={'/help'}>Help</NavLink></li>
-          </ul>
-        </nav>
+        <Menu mode="horizontal">
+          <Menu.Item key="home">
+            <NavLink to={'/'}>Home</NavLink>
+          </Menu.Item>
+          <Menu.Item key="done">
+            <NavLink to={'/done'}>Done List</NavLink>
+          </Menu.Item>
+          <Menu.Item key="help">
+            <NavLink to={'/help'}>Help</NavLink>
+          </Menu.Item>
+        </Menu>
       </div>
       <Outlet />
     </>
--- src/components/TodoGroup.js
+++ src/components/TodoGroup.js
@@ -3,10 +3,10 @@ import TodoItem from "./TodoItem";
 import "./css/TodoGroup.css";
 
 const TodoGroup = (props) => {
-    const todoTask = useSelector(state => state.todo.todoList)
+    const todoList = props.todoList;
     return (
         <ul className="todoGroup">
-            {(props.isDone ? todoTask.filter(task=> task.done) : todoTask).map((todo) => (
+            {(props.isDone ? todoList.filter(task=> task.done) : todoList).map((todo) => (
                 <TodoItem key={todo.id} todo={todo} />
             ))}
         </ul>
